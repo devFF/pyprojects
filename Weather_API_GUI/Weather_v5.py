56bc6767ec28d59fec5939d2f7b4b41042d3f87f
Igor Yunusov <56348557+devFF@users.noreply.github.com>
2021-10-28 17:04:38 +0500

    v5

--- Weather_API_GUI/Weather_v5.py
+++ Weather_API_GUI/Weather_v5.py
@@ -11,6 +11,12 @@ from PIL import Image, ImageTk
 from io import BytesIO
 import datetime
 import shutil
+import matplotlib
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+import matplotlib.ticker as ticker
+import pandas as pd
+import numpy as np
 
 
 class App:
@@ -21,8 +27,8 @@ class App:
         # Узнаем имя пользователя
         name = os.getlogin()
 
-        # Костыль, задаем количеством строчек Label: используется для их обновления
-        self.num_label = 10
+        # Костыль, задаем количеством строчек Label + 1(картинка температуры): используется для их обновления
+        self.num_label = 11
 
         # Узнаем местоположение пользователя
         self.city = self.user_location()
@@ -35,8 +41,22 @@ class App:
 
         # Создаем рабочую область и тему
         self.window = tk.Tk()
-        self.window.title("Weather caster")
-        self.window.geometry('720x320')
+        self.window.title("Weather forecast")
+        w_height, w_width = 645, 775
+        self.window.geometry('{}x{}'.format(w_height,w_width))
+        self.window.resizable(False, False)  # Запрещаем менять размер окна
+        self.bg_color = '#02D0E3'
+        self.window['bg'] = self.bg_color
+
+        # Централизуем положения окна
+        self.window.withdraw()
+        self.window.update_idletasks()  # Update "requested size" from geometry manager
+        print(self.window.winfo_screenwidth(), self.window.winfo_reqwidth())
+        x = (self.window.winfo_screenwidth() - w_width) / 2
+        y = (self.window.winfo_screenheight() - w_height) / 2
+        self.window.wm_geometry("+%d+%d" % (x, y))
+        self.window.deiconify()
+
         # self.window.tk.call("source", "sun-valley.tcl")
         # self.window.tk.call("set_theme", "light")
 
@@ -47,7 +67,7 @@ class App:
         self.greetings = tk.Label(self.window,
                                   text='Select city:',
                                   font=('Times New Roman', 20),
-                                  justify='left', width=9)
+                                  justify='left', width=9, bg=self.bg_color)
         self.greetings.grid(column=0, row=0, sticky='w')
 
         # Получаем список городов России и добавляем текущий город в общий список
@@ -77,12 +97,11 @@ class App:
         # Главный цикл программы
         self.window.mainloop()
 
-
-    def weather_info(self, event = None):
+    def weather_info(self, event=None):
         """Обновляет информацию о погоде"""
         self.city = self.city_box.get()
-        self.weather = self.my_weather(city=self.city)  # Текущая погода
-        self.weather_forecast = self.my_weather_forecast(city=self.city)  # Прогноз погоды
+        self.weather = self.my_weather_forecast(forecast=False, city=self.city)  # Текущая погода
+        self.weather_forecast = self.my_weather_forecast(forecast=True, city=self.city)  # Прогноз погоды
         self.temperature = self.get_temperature(value='temp')  # Текущая температура
         self.real_temperature = self.get_temperature(value='feels_like')  # Текущая реальная температура
         # Удаляем информацию о предыдущем городе чтобы не было наслоения данных друг на друга
@@ -94,25 +113,32 @@ class App:
                     break
         weather_data = tk.Label(
             self.window,
-            text='Now temperature in {} is {} C, feels like {} C'.format(self.city,
-                                                                         self.temperature,
+            text='Now temperature is {}° C, feels like {}° C'.format(self.temperature,
                                                                          self.real_temperature),
-            font=('Times New Roman', 18), justify='left')
+            font=('Times New Roman', 18), justify='left', bg=self.bg_color)
         weather_data.grid(column=0, row=1, sticky='w')
 
         # Обрабатываем прогноз погоды, выводим текст прогноза
         self.get_temperature_forecast()
 
+        # Тест графика
+        self.plot()
+
         # Отображаем иконки погоды
-        img_list = ['test.png', 'day0.png', 'evening0.png', 'day1.png', 'evening1.png']
+        img_list = ['test.png', 'day0.png', 'evening0.png', 'day1.png', 'evening1.png', 'plt.png']
         all_lebels = []  # Используется чтобы коллектор мусора пайтона не убирал из памяти иконки погоды
         for i in range(len(img_list)):
             image = ImageTk.PhotoImage(Image.open(img_list[i]))
-            label = tk.Label(image=image)
-            label.photo = image
-            label.grid(row=i+1, column=1)
-            all_lebels.append(label)
 
+            if 'plt' not in img_list[i]:
+                label = tk.Label(image=image, bg=self.bg_color)
+                label.photo = image
+                label.grid(row=i + 1, column=1)
+            else:
+                label = tk.Label(image=image, justify='left', bg=self.bg_color)
+                label.photo = image
+                label.grid(row=15, columnspan=2)
+            all_lebels.append(label)
 
     def city_list(self):
         """Парсинг html-таблицы с использованием библиотеки BeautifulSoup. Таблица состоит из 5 столбцов:
@@ -124,7 +150,7 @@ class App:
                 lines = r.readlines()
                 for line in lines:
                     city_base.append(line.replace('\n', ''))
-        except:
+        except FileNotFoundError:
             url = 'https://on55.ru/articles/2'
             response = requests.get(url)
             soup = BeautifulSoup(response.text, 'lxml')
@@ -134,9 +160,9 @@ class App:
                     city_base.append(items[i].get_text())
             with open('city_base.txt', 'w') as w:
                 for city in city_base:
-                    w.writelines("%s\n" % city)
+                    if 'Оспаривается' not in city:
+                        w.writelines("%s\n" % city)
         return city_base
-
 
     def read_api(self):
         """return str with API from file API.txt"""
@@ -146,7 +172,6 @@ class App:
             return api
         except:
             print('Файл API.txt не был найден')
-
 
     def user_location(self):
         """Determine user location, return name of the city in STR"""
@@ -158,52 +183,46 @@ class App:
                 loc = loc['properties']['city']
         return loc
 
-
-    def my_weather(self, city):
-        """Use openweathermap.org to get current weather in city
-        Return content of response"""
-        API_KEY = self.read_api()
-        url = 'https://api.openweathermap.org/data/2.5/weather?q={}&appid={}'.format(city, API_KEY)
-        r = requests.get(url)
-        #content = r.text
-        content_json = r.json()
-        return (content_json)
-
-    def my_weather_forecast(self, city):
-        """Use openweathermap.org to get weather forecast in city
+    def my_weather_forecast(self, forecast, city):
+        """Use openweathermap.org to get current weather and weather forecast in city
                 Return content of response"""
         API_KEY = self.read_api()
-        url = 'https://api.openweathermap.org/data/2.5/forecast?q={}&appid={}'.format(city, API_KEY)
+        if forecast:
+            url = 'https://api.openweathermap.org/data/2.5/forecast?q={}&appid={}'.format(city, API_KEY)
+        else:
+            url = 'https://api.openweathermap.org/data/2.5/weather?q={}&appid={}'.format(city, API_KEY)
         r = requests.get(url)
         content_json = r.json()
-        print(content_json)
         return (content_json)
-
 
     def get_temperature(self, value):
         """Determine temperature from weather"""
-        ORDER = 2
+        ORDER = 1
         try:
             temp_data_in_K = self.weather['main'][value]
             temp_data_in_C = round((temp_data_in_K - 273.15), ORDER)
             return temp_data_in_C
         except Exception as ex:
-            messagebox.showinfo(title='Error', message='Got data error')
-            print('Входные данные вызвали ошибку ', type(ex))
-            return "\nНе удалось узнать {}".format(value)
+            messagebox.showinfo(title='Error', message='Got data error: \n {}: {}'.format(ex.__class__, ex))
 
     def get_temperature_forecast(self):
         """Определяем прогноз погоды на два дня вперед в 15:00 и 21:00, скачиваем иконки, обновляем текст в окне
         Иконки обновляются в функции weather_info"""
-        ORDER = 2
+        ORDER = 1
         day_temperature_list, evening_temperature_list = [], []
         day_feel_temperature_list, evening_feel_temperature_list = [], []
         tomorrow = str(self.weather_forecast['list'][8]['dt_txt']).split()[0]  # Дата завтрашнего дня, убираем часы мин
         day_after_tomorrow = str(self.weather_forecast['list'][16]['dt_txt']).split()[0]  # Дата послезавтра
         days = [tomorrow, day_after_tomorrow]
+        self.all_data = []
+        self.all_temp = []
+        self.all_real_temp = []
         try:
             counter = 0
             for i in range(len(self.weather_forecast['list'])):
+                self.all_data.append(self.weather_forecast['list'][i]['dt_txt'])
+                self.all_temp.append(round((self.weather_forecast['list'][i]['main']['temp'] - 273.15), ORDER))
+                self.all_real_temp.append(round((self.weather_forecast['list'][i]['main']['feels_like'] - 273.15), ORDER))
                 if '15:00:00' in self.weather_forecast['list'][i]['dt_txt']:
                     if self.current_data not in self.weather_forecast['list'][i]['dt_txt'] and counter < 2:
                         temp_data_in_K = self.weather_forecast['list'][i]['main']['temp']
@@ -242,23 +261,22 @@ class App:
             for i in range(2):
                 weather_data_day = tk.Label(
                     self.window,
-                    text='{} day temperature is {} C, feels like {} C'.format(days[i],
-                                                                                 day_temperature_list[i],
-                                                                                 day_feel_temperature_list[i]),
-                    font=('Times New Roman', 18), justify='left')
-                weather_data_day.grid(column=0, row=2*i+2, sticky='w')
+                    text='{} day temperature is {}° C, feels like {}° C'.format(days[i],
+                                                                              day_temperature_list[i],
+                                                                              day_feel_temperature_list[i]),
+                    font=('Times New Roman', 18), justify='left',bg=self.bg_color)
+                weather_data_day.grid(column=0, row=2 * i + 2, sticky='w')
 
                 weather_data_evening = tk.Label(
                     self.window,
-                    text='{} evening temperature is {} C, feels like {} C'.format(days[i],
-                                                                              evening_temperature_list[i],
-                                                                              evening_feel_temperature_list[i]),
-                    font=('Times New Roman', 18), justify='left')
-                weather_data_evening.grid(column=0, row=2*i+3, sticky='w')
+                    text='{} evening temperature is {}° C, feels like {}° C'.format(days[i],
+                                                                                  evening_temperature_list[i],
+                                                                                  evening_feel_temperature_list[i]),
+                    font=('Times New Roman', 18), justify='left', bg=self.bg_color)
+                weather_data_evening.grid(column=0, row=2 * i + 3, sticky='w')
 
         except Exception as ex:
-            messagebox.showinfo(title='Error', message='Got data error')
-
+            messagebox.showinfo(title='Error', message='Got data error: \n {}: {}'.format(ex.__class__, ex))
 
     def get_ico(self):
         """Скачиваем иконку для текущей погоды"""
@@ -268,8 +286,8 @@ class App:
             response = requests.get(ico_url)
             img = Image.open(BytesIO(response.content))
             img.save('test.png')
-        except:
-            messagebox.showinfo(title='Error', message='Image download error')
+        except Exception as ex:
+            messagebox.showinfo(title='Error', message='Got data error: \n {}: {}'.format(ex.__class__, ex))
 
     def get_current_data(self):
         """Определяем какое сегодня число и выводим год-месяц-день"""
@@ -281,14 +299,64 @@ class App:
         """Создаем директорию для временных файлов и переходим в нее"""
         try:
             os.mkdir('Temp')
-        except:
+        except FileExistsError:
             pass
         try:
             shutil.copy2('API.txt', 'Temp')
-        except:
+        except FileNotFoundError:
             pass
+
         os.chdir('Temp')
+
+    def funcForFormatter(self, x, pos):
+        date_list_night = np.arange(5,38,8)
+        date_list_day = np.arange(9,34,8)
+        print(date_list_day, date_list_night)
+        x = int(x)
+
+        if x in date_list_night:
+            date = str(self.all_data[x-1]).split()
+            print(date)
+            return u'{}\n{}'.format(date[0][5:],date[1][:-3])  # Используем срез, чтобы обрезать год и секунды
+
+        if x in date_list_day:
+            date = str(self.all_data[x-1]).split()
+            print(date)
+            return u'{}\n{}'.format(date[0][5:],date[1][:-3])  # Используем срез, чтобы обрезать год и секунды
+
+
+    def plot(self):
+        fig = plt.figure(figsize=(6.45, 4.8), dpi=100)
+        ax = fig.add_subplot(111)
+        fig.patch.set_facecolor(self.bg_color)
+        ax.patch.set_facecolor(self.bg_color)
+        xdata = np.arange(0,len(self.all_data),1)
+        xmin, xmax = xdata[5], xdata[-3]  # удаляем прогнозы на текущий день и на начало 5-го дня
+        ymin, ymax = min(self.all_real_temp), max(self.all_temp)
+
+        # Создаем форматер
+        ax.xaxis.set_major_locator(ticker.MultipleLocator(1))
+        """Информация о FuncFormatter: позволяет гибко настраивать формат меток с помощью функции, 
+        которая будет возвращать строковое представление каждой метки"""
+        formatter = matplotlib.ticker.FuncFormatter(self.funcForFormatter)
+
+        # Установка форматера для оси X
+        ax.xaxis.set_major_formatter(formatter)
+        ax.set_xlim(xmin, xmax)
+        ax.set_ylim(ymin, ymax)
+        ax.set_title("Four day weather forecast", fontsize=14, color='black')
+        ax.set_ylabel("Temperature, [C]", fontsize=14, color='black')
+
+        for i in np.arange(5,38,8):
+            ax.vlines(i, ymin, ymax, color='black')
+        for i in np.arange(9,34,8):
+            ax.vlines(i, ymin, ymax, color='black', linestyles='--')
+        ax.plot(xdata, self.all_temp, label = '$T$', color = 'red')
+        ax.plot(xdata, self.all_real_temp, label ='$T_{feel}$', color = '#0F27FF')
+        plt.legend()
+        plt.savefig('plt.png')
+        plt.close()
+
 
 if __name__ == '__main__':
     app = App()
-
